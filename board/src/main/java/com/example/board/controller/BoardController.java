d2cd9fd28f64e7f705674859df05407009dec2ce
crpark <myanjini@gmail.com>
2026-03-22 08:42:34 +0900

    게시판 상세 조회 기능 구현

--- board/src/main/java/com/example/board/controller/BoardController.java
+++ board/src/main/java/com/example/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.board.dto.BoardDto;
@@ -13,30 +14,41 @@ import com.example.board.service.BoardService;
 
 @Controller
 public class BoardController {
-	@Autowired
-	BoardService boardService;
+    @Autowired
+    BoardService boardService;
 
-	@GetMapping("/board/openBoardList.do")
-	public ModelAndView openBoardList() throws Exception {
-		ModelAndView mv = new ModelAndView("/board/boardList");
+    @GetMapping("/board/openBoardList.do")
+    public ModelAndView openBoardList() throws Exception {
+        ModelAndView mv = new ModelAndView("/board/boardList");
 
-		List<BoardDto> list = boardService.selectBoardList();
-		mv.addObject("list", list);
+        List<BoardDto> list = boardService.selectBoardList();
+        mv.addObject("list", list);
 
-		return mv;
-	}
+        return mv;
+    }
 
-	// 글쓰기 화면 요청을 처리하는 메서드
-	@GetMapping("/board/openBoardWrite.do")
-	public String openBoardWrite() throws Exception {
-		return "/board/boardWrite";
-	}
+    // 글쓰기 화면 요청을 처리하는 메서드
+    @GetMapping("/board/openBoardWrite.do")
+    public String openBoardWrite() throws Exception {
+        return "/board/boardWrite";
+    }
 
-	// 글 저장 요청을 처리하는 메서드
-	@PostMapping("/board/insertBoard.do")
-	public String insertBoard(BoardDto boardDto) throws Exception {
-		boardService.insertBoard(boardDto);
-		return "redirect:/board/openBoardList.do";
-	}
+    // 글 저장 요청을 처리하는 메서드
+    @PostMapping("/board/insertBoard.do")
+    public String insertBoard(BoardDto boardDto) throws Exception {
+        boardService.insertBoard(boardDto);
+        return "redirect:/board/openBoardList.do";
+    }
+
+    // 상세 조회 요청을 처리하는 메서드
+    @GetMapping("/board/openBoardDetail.do")
+    public ModelAndView openboardDetail(@RequestParam("boardIdx") int boardIdx) throws Exception {
+        BoardDto boardDto = boardService.selectBoardDetail(boardIdx);
+
+        ModelAndView mv = new ModelAndView("/board/boardDetail");
+        mv.addObject("board", boardDto);
+
+        return mv;
+    }
 
 }
--- board/src/main/java/com/example/board/service/BoardServiceImpl.java
+++ board/src/main/java/com/example/board/service/BoardServiceImpl.java
@@ -10,18 +10,24 @@ import com.example.board.mapper.BoardMapper;
 
 @Service
 public class BoardServiceImpl implements BoardService {
-	@Autowired
-	BoardMapper boardMapper;
-	
-	@Override
-	public List<BoardDto> selectBoardList() {
-		return boardMapper.selectBoardList();
-	}
+    @Autowired
+    BoardMapper boardMapper;
 
-	@Override
-	public void insertBoard(BoardDto boardDto) {
-		boardDto.setCreatedId("admin");
-		boardMapper.insertBoard(boardDto);		
-	}
+    @Override
+    public List<BoardDto> selectBoardList() {
+        return boardMapper.selectBoardList();
+    }
+
+    @Override
+    public void insertBoard(BoardDto boardDto) {
+        boardDto.setCreatedId("admin");
+        boardMapper.insertBoard(boardDto);
+    }
+
+    @Override
+    public BoardDto selectBoardDetail(int boardIdx) {
+        boardMapper.updateHitCnt(boardIdx);
+        return boardMapper.selectBoardDetail(boardIdx);
+    }
 
 }
