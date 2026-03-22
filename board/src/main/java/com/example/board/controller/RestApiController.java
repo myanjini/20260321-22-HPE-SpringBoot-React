5c90871c061adada05860477eb082bf117ee08b3
crpark <myanjini@gmail.com>
2026-03-22 10:18:57 +0900

    상태 코드 반환

--- board/src/main/java/com/example/board/controller/RestApiController.java
+++ board/src/main/java/com/example/board/controller/RestApiController.java
@@ -1,8 +1,12 @@
 package com.example.board.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +38,17 @@ public class RestApiController {
     
     // 상세 조회 요청을 처리하는 메서드
     @GetMapping("/board/{boardIdx}")
-    public BoardDto openboardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception {
-        return boardService.selectBoardDetail(boardIdx);
+    public ResponseEntity<Object> openboardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception {
+        BoardDto dto = boardService.selectBoardDetail(boardIdx);
+        if (dto == null) {
+            Map<String, Object> result = new HashMap<>();
+            result.put("code", HttpStatus.NOT_FOUND.value());
+            result.put("name", HttpStatus.NOT_FOUND.name());
+            result.put("message", "게시판 번호 " + boardIdx + "와 일치하는 게시물이 존재하지 않습니다.");
+            
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(dto);
+        }
     }
 }
